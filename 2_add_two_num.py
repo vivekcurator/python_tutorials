a70769130fd19b6eb2f879301bc196e7af9db373
vivekcurator <sainivivek62@gmail.com>
2022-12-10 00:40:01 +0530

    practice 2

---
+++ 2_add_two_num.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 00:18:10 2022
+
+@author: viveksaini
+"""
+
+a = 10
+b = 20
+
+print("Sum of a and b is", a+b)
+print(" ")
+
+##another method
+a = input("Enter the first number")
+b = input("Enter the second number")
+c = a+b
+ 
+print("The sum of "+a+" and "+b+" is "+c)
