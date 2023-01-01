3ede599c7c8f611af0c6ba30c938884ca69f74da
vivekcurator <sainivivek62@gmail.com>
2023-01-01 20:07:09 +0530

    latest commit

--- 2_user_entered_sum.py
+++ 2_user_entered_sum.py
@@ -6,8 +6,8 @@ Created on Tue Dec 13 00:11:30 2022
 @author: viveksaini
 """
 
-num1 = int(input("Enter the first number "))
-num2 = int(input("Enter the second number "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 sum = num1 + num2
  
-print('The sum of the numbers =', sum)ßßßßß
+print('The sum of the numbers =', sum)
---
+++ good_afternoon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  1 19:53:58 2023
+
+@author: viveksaini
+"""
+
+name = input("Please enter your name: ")
+print("Good Afternoon!", name)
